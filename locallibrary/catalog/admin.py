2a5747f3ae1d84357f0bc20dadf077346d6f1110
Leander Perera <ltcperera@gmail.com>
2019-09-24 22:49:41 -0700

    Added customizations for admin views

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
+# Define admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+            (None, {
+                'fields': ('book', 'imprint', 'id')
+            }),
+            ('Availability', {
+                'fields': ('status', 'due_back')
+            }),
+        )
+
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
 admin.site.register(Language)
