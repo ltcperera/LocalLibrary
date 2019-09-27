d04fc0d37c8eef526328f27a10a117bd08345c50
Leander Perera <ltcperera@gmail.com>
2019-09-27 09:53:24 -0700

    Changed the way book instance objects are represented.

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -78,7 +78,7 @@ class BookInstance(models.Model):
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.id} ({self.book.title})'
+        return '{0} ({1})'.format(self.id, self.book.title)
 
 class Author(models.Model):
     """Model representing an author."""
